7c47776ee9dc5c350fe2c84b9b7ef7b401a59030
kazuooooo <matsumotokazuya7@gmail.com>
2020-06-29 13:07:13 +0900

    :tada: Add starter

---
+++ CocoaMVCFromScratch/CocoaMVCFromScratch/Controller/CounterViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  CocoaMVCFromScratch
+//
+//  Created by 松本和也 on 2020/06/29.
+//  Copyright © 2020 松本和也. All rights reserved.
+//
+
+import UIKit
+
+class CounterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
